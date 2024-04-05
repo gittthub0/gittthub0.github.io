5db22fb9af7417777c094a676c7f60c6a6733d07
gittthub0 <rahmatenomoto687@gmail.com>
2024-04-05 15:09:28 +0700

    ke 2

---
+++ home.js
@@ -0,0 +1,9 @@
+const subsribe=document.querySelector('.buttons .button')
+subsribe.addEventListener('click',function(){
+    subsribe.style.color='lightskyblue'
+})
+
+const like=document.querySelector('.bx.bx-heart')
+like.addEventListener('click',function(){
+    like.style.color='red'
+})
